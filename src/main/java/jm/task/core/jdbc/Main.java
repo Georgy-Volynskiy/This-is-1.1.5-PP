a26acb1d2af10e9ffb87f0a72b3b4634e45b7d76
Georgy-Volynskiy <126697927+Georgy-Volynskiy@users.noreply.github.com>
2023-04-24 15:23:42 +0300

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+//        Util.getConnection();
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Petr1", "LastPetr1", (byte) 8);
+        userService.saveUser("Petr2", "LastPetr2", (byte) 16);
+        userService.saveUser("Petr3", "LastPetr3", (byte) 32);
+        userService.saveUser("Petr4", "LastPetr4", (byte) 64);
+
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
